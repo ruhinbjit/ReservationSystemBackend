444352ed64d08224a03b9f987beb7eb404c621c9
ShamimRayhan <shamimrayhanuap@gmail.com>
2022-01-20 15:01:56 +0600

    Add files via upload
    Initial project setup

---
+++ src/main/java/com/example/ReservationSystemBackend/ReservationSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReservationSystemBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ReservationSystemBackend/ReservationSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReservationSystemBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
